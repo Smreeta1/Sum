b0c79bd619247f82afb784379c66a4469f9f5ba4
Smreeta Shrestha <smreetashrestha1@gmail.com>
2023-11-01 08:15:39 +0545

    Initial commit

---
+++ using_asy-awa.js
@@ -0,0 +1,23 @@
+function sumOfNumbersPromise(numbers) {
+    return new Promise((resolve, reject) => {
+      if (numbers.length !== 4) {
+        reject(" 4 numbers are required!");
+      } else {
+        let sum = 0;
+        for (const num of numbers) {
+          sum += num;
+        }
+        resolve(sum);
+      }
+    });
+  }
+  
+  const numbers = [1, 2, 3, 4];
+  sumOfNumbersPromise(numbers)
+    .then((result) => {
+      console.log("Sum:", result);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  
